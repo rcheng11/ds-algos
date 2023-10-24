fb0a3caa64834e0925328016e64e25c1de314a4f
Ronald Cheng <ronaldcheng11@gmail.com>
2023-10-24 19:30:08 -0400

    fixed intro_graphs.py

--- intro_graphs.py
+++ intro_graphs.py
@@ -9,19 +9,34 @@ Design a matrix implementation of a Graph with the functions
 
 class MatrixGraph:
     def __init__(self, matrix):
+        '''
+        The matrix is a 2D square array with n and m = |V|
+        Each row in the matrix represents a vertex
+            The column at that row represents whether there is a directed edge
+            from that row's vertex to that column's vertex. (1 if edge, 0 if not)
+        
+        Ex:
+        [0 0 1]
+        [0 0 1]
+        [1 0 0]
+        1 -- 3 <-- 2
+        '''
         self.matrix = matrix
 
-    def run_bfs(v: tuple) -> bool:
-        '''Runs BFS on the Graph object. Returns True if v found, False otherwise.
-        Implementation Notes:
+    def run_bfs(self, s: int, v: int) -> bool:
+        '''Runs BFS on the Graph object. Returns True if s reaches v, False otherwise.
+        Implementation Notes: Start from s, go one vertex away at a time until all are visited
             Base Cases:
             Logic:
         Runtime: 
         '''
-        pass
+        visited = []
 
-    def run_dfs(v: tuple) -> bool:
-        '''Runs DFS on the Graph object. Returns True if v found, False otherwise.
+        while len(visited) < len(self.matrix):
+            pass
+
+    def run_dfs(self, s: int, v: int) -> bool:
+        '''Runs DFS on the Graph object. Returns True if s reaches v, False otherwise.
         Implementation Notes:
             Base Cases:
             Recursive Relation:
@@ -30,7 +45,7 @@ class MatrixGraph:
         '''
         pass
 
-    def count_paths(v: tuple) -> int:
+    def count_paths(self, v: int) -> int:
         '''Counts the number of paths on the Graph object.
         Implementation Notes:
             Base Cases:
@@ -40,9 +55,9 @@ class MatrixGraph:
         '''
         pass
 
-    def sort_degree(s: tuple) -> list[tuple]:
+    def sort_degree(self, s: int) -> list[int]:
         '''Returns a list of tuples representing vertices
-        i.e (r,c) on the matrix by ascending order of the
+        as integers on the matrix by ascending order of the
         degree of the vertex in the graph from a source
         vertex s.
         Implementation Notes: 
--- test_intro_graphs.py
+++ test_intro_graphs.py
@@ -1,3 +1,10 @@
 from intro_graphs import MatrixGraph
-
+import pytest
 # Test cases
+
+def test_create_matrix():
+    graph = [[0, 0, 0, 1],
+             [0, 0, 0, 1],
+             [0, 0, 0, 1],
+             [0, 0, 0, 1]]
+    
